ad74aa5a3d9590239869a253308c5026da01d4a8
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-02-24 19:02:54 +0100

    add carousel

--- portfolio/src/routes/[[lang]]/+page.svelte
+++ portfolio/src/routes/[[lang]]/+page.svelte
@@ -6,12 +6,40 @@
         projects_highlighted_title,
         projects_highlighted_description,
     } from "$lib/paraglide/messages.js";
+    import * as Carousel from "$lib/components/ui/carousel";
+    import * as Card from "$lib/components/ui/card";
 </script>
 
 <Hero />
 <Profile />
 
-<section class="container mx-auto px-3 my-8 prose dark:prose-invert">
-    <h2>{projects_highlighted_title()}</h2>
-    <p>{projects_highlighted_description()}</p>
+<section class="px-3 my-8 py-8 bg-card w-full">
+    <div class="container mx-auto">
+        <div class="prose dark:prose-invert max-w-none mb-8">
+            <h2 class="text-center">{projects_highlighted_title()}</h2>
+            <p class="text-center">{projects_highlighted_description()}</p>
+        </div>
+
+        <div class="relative px-12">
+            <Carousel.Root opts={{ align: "center" }}>
+                <Carousel.Content class="ms-0 gap-4">
+                    {#each Array(3) as _}
+                        <Carousel.Item class="basis-auto lg:p-4">
+                            <Card.Root
+                                class="border-2 border-dashed flex items-center justify-center opacity-50 w-80 h-96 bg-background/50"
+                            >
+                                <Card.Content class="p-0">
+                                    <span class="text-muted-foreground"
+                                        >Project Card</span
+                                    >
+                                </Card.Content>
+                            </Card.Root>
+                        </Carousel.Item>
+                    {/each}
+                </Carousel.Content>
+                <Carousel.Previous class="md:hidden" />
+                <Carousel.Next class="md:hidden" />
+            </Carousel.Root>
+        </div>
+    </div>
 </section>
